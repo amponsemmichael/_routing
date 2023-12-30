8bc0bc9f1f59b33fd960ee129fa019d931db58a9
amponsem michael <amponsem_michael@yahoo.com>
2023-12-30 02:04:18 +0000

    "dynamic route example"

--- app/projects/lists/page.tsx
+++ app/projects/lists/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import styles from "./projects.module.css";
+
 export default function ProjectList() {
   return (
     <main>
       <h1>ProjectList</h1>
+
+      <ul className={styles.ul}>
+        <li>
+          <Link href="/projects/mart">Lizmart</Link>
+        </li>
+        <li>
+          <Link href="/projects/profile">Michael Amponsem</Link>
+        </li>
+        <li>
+          <Link href="/projects/blog">Educational Info</Link>
+        </li>
+      </ul>
     </main>
   );
 }
